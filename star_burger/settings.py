a661e878325c06dddfc736d40e9a2ac03a03e4b0
learn2410 <learn2410@mail.ru>
2022-06-23 23:08:42 +0500

    Remove default SECRET_KEY (now  only from environment)

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -9,7 +9,7 @@ env.read_env()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-SECRET_KEY = env('SECRET_KEY', 'etirgvonenrfnoerngorenogneongg334g')
+SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
